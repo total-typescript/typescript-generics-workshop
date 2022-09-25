95176dd62ef2f4aa5402e7cbe5d1c7c830dd4118
Matt Pocock <mattpocockvoice@gmail.com>
2022-09-25 21:16:48 +0100

    WIP

---
+++ src/04-function-overloads/21-generics-in-function-overloads.solution.ts
@@ -0,0 +1,26 @@
+import { it } from "vitest";
+import { Equal, Expect } from "../helpers/type-utils";
+
+function returnWhatIPassInExceptFor1(t: 1): 2;
+function returnWhatIPassInExceptFor1<T>(t: T): T;
+function returnWhatIPassInExceptFor1(t: unknown): unknown {
+  return t;
+}
+
+it("Should return the type 2 when you pass in 1", () => {
+  const result = returnWhatIPassInExceptFor1(1);
+
+  type test1 = Expect<Equal<typeof result, 2>>;
+});
+
+it("Otherwise, should return what you pass in", () => {
+  const a = returnWhatIPassInExceptFor1("a");
+  const b = returnWhatIPassInExceptFor1("b");
+  const c = returnWhatIPassInExceptFor1("c");
+
+  type tests = [
+    Expect<Equal<typeof a, "a">>,
+    Expect<Equal<typeof b, "b">>,
+    Expect<Equal<typeof c, "c">>
+  ];
+});
---
+++ src/06-challenges/28-pick.solution.ts
@@ -0,0 +1,40 @@
+import { expect, it } from "vitest";
+import { Equal, Expect } from "../helpers/type-utils";
+
+const pick = <TObj, TPicked extends keyof TObj>(obj: TObj, pick: TPicked[]) => {
+  return pick.reduce((acc, key) => {
+    acc[key] = obj[key];
+    return acc;
+  }, {} as Pick<TObj, TPicked>);
+};
+
+it("Should pick the keys from the object", () => {
+  const result = pick(
+    {
+      a: 1,
+      b: 2,
+      c: 3,
+    },
+    ["a", "b"]
+  );
+
+  expect(result).toEqual({ a: 1, b: 2 });
+
+  type test = Expect<Equal<typeof result, { a: number; b: number }>>;
+});
+
+it("Should not allow you to pass keys which do not exist in the object", () => {
+  pick(
+    {
+      a: 1,
+      b: 2,
+      c: 3,
+    },
+    [
+      "a",
+      "b",
+      // @ts-expect-error
+      "d",
+    ]
+  );
+});
