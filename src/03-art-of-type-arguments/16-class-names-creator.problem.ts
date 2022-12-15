61c4ebccf428723e2581904d94ea7942f5f79015
Matt Pocock <mattpocockvoice@gmail.com>
2022-12-15 13:15:19 +0000

    Added a test to 16

--- src/03-art-of-type-arguments/16-class-names-creator.problem.ts
+++ src/03-art-of-type-arguments/16-class-names-creator.problem.ts
@@ -1,4 +1,5 @@
 import { expect, it } from "vitest";
+import { Equal, Expect } from "../helpers/type-utils";
 
 const createClassNamesFactory =
   (classes: unknown) =>
@@ -19,11 +20,17 @@ it("Should let you create classes from a className factory", () => {
 
 it("Should let you pass additional classes which get appended", () => {
   expect(getBg("primary", "text-white", "rounded", "p-4")).toEqual(
-    "bg-blue-500 text-white rounded p-4",
+    "bg-blue-500 text-white rounded p-4"
   );
 });
 
-it("Should not let you pass invalid classes", () => {
+it("Should return a type of string", () => {
+  const result = getBg("primary");
+
+  type test = Expect<Equal<typeof result, string>>;
+});
+
+it("Should not let you pass invalid variants", () => {
   // @ts-expect-error
   getBg("123123");
 });
--- src/03-art-of-type-arguments/16-class-names-creator.solution.2.ts
+++ src/03-art-of-type-arguments/16-class-names-creator.solution.2.ts
@@ -19,11 +19,17 @@ it("Should let you create classes from a className factory", () => {
 
 it("Should let you pass additional classes which get appended", () => {
   expect(getBg("primary", "text-white", "rounded", "p-4")).toEqual(
-    "bg-blue-500 text-white rounded p-4",
+    "bg-blue-500 text-white rounded p-4"
   );
 });
 
-it("Should not let you pass invalid classes", () => {
+it("Should return a type of string", () => {
+  const result = getBg("primary");
+
+  type test = Expect<Equal<typeof result, string>>;
+});
+
+it("Should not let you pass invalid variants", () => {
   // @ts-expect-error
   getBg("123123");
 });
