d125ce3dbf8ad0dc156d96624852aa6c62ff7c99
Matt Pocock <mattpocockvoice@gmail.com>
2022-12-15 12:17:26 +0000

    Added 17.5

---
+++ src/04-generics-advanced/17.5-inference-inside-generic-functions.problem.ts
@@ -0,0 +1,16 @@
+type Person = {
+  name: string;
+  age: number;
+  birthdate: Date;
+};
+
+export function remapPerson<Key extends keyof Person>(
+  key: Key,
+  value: Person[Key],
+): Person[Key] {
+  if (key === "birthdate") {
+    return new Date();
+  }
+
+  return value;
+}
