2fda31617809742d072781ee54c25f8900f2839e
Mehdi Raash <leaksin@gmail.com>
2022-12-25 23:03:43 -0500

    feat: add additional solutions to 06 exercise

---
+++ src/01-generics-intro/06-generic-mapper.solution.3.ts
@@ -0,0 +1,94 @@
+import { expect, it } from "vitest";
+import { Equal, Expect } from "../helpers/type-utils";
+
+interface FirstName {
+  firstName: string;
+}
+
+interface LastName {
+  lastName: string;
+}
+
+export const concatenateFirstNameAndLastName = <T extends FirstName & LastName>(
+  user: T
+) => {
+  return {
+    ...user,
+    fullName: `${user.firstName} ${user.lastName}`,
+  };
+};
+
+it("Should add fullName to an object which only contains firstName and lastName", () => {
+  const users = [
+    {
+      firstName: "Matt",
+      lastName: "Pocock",
+    },
+  ];
+
+  const newUsers = users.map(concatenateFirstNameAndLastName);
+
+  expect(newUsers).toEqual([
+    {
+      firstName: "Matt",
+      lastName: "Pocock",
+      fullName: "Matt Pocock",
+    },
+  ]);
+
+  type tests = [
+    Expect<
+      Equal<
+        typeof newUsers,
+        Array<{ firstName: string; lastName: string } & { fullName: string }>
+      >
+    >
+  ];
+});
+
+it("Should retain other properties passed in", () => {
+  const users = [
+    {
+      id: 1,
+      firstName: "Matt",
+      lastName: "Pocock",
+    },
+  ];
+
+  const newUsers = users.map(concatenateFirstNameAndLastName);
+
+  expect(newUsers).toEqual([
+    {
+      id: 1,
+      firstName: "Matt",
+      lastName: "Pocock",
+      fullName: "Matt Pocock",
+    },
+  ]);
+
+  type tests = [
+    Expect<
+      Equal<
+        typeof newUsers,
+        Array<
+          { id: number; firstName: string; lastName: string } & {
+            fullName: string;
+          }
+        >
+      >
+    >
+  ];
+});
+
+it("Should fail when the object passed in does not contain firstName", () => {
+  const users = [
+    {
+      firstName: "Matt",
+    },
+  ];
+
+  const newUsers = users.map(
+    // @ts-expect-error
+    concatenateFirstNameAndLastName
+  );
+});
