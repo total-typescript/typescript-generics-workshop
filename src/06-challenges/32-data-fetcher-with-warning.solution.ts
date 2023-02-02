393d7f411215c2fb8c76e793508f5986ada0f211
Matt Pocock <mattpocockvoice@gmail.com>
2023-02-02 10:56:21 +0000

    Changed solution of 32

--- src/06-challenges/32-data-fetcher-with-warning.solution.ts
+++ src/06-challenges/32-data-fetcher-with-warning.solution.ts
@@ -1,20 +1,18 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const fetchData = async <TResult>(
-  url: string,
-): Promise<
-  unknown extends TResult
-    ? "You must pass a type argument to fetchData"
-    : TResult
-> => {
+const fetchData = async <
+  TResult = "You must pass a type argument to fetchData"
+>(
+  url: string
+): Promise<TResult> => {
   const data = await fetch(url).then((response) => response.json());
   return data;
 };
 
 it("Should fetch data from an API", async () => {
   const data = await fetchData<{ name: string }>(
-    "https://swapi.dev/api/people/1",
+    "https://swapi.dev/api/people/1"
   );
   expect(data.name).toEqual("Luke Skywalker");
 
@@ -25,6 +23,6 @@ it("Should force you to add a type annotation", async () => {
   const data = await fetchData("https://swapi.dev/api/people/1");
 
   type tests = [
-    Expect<Equal<typeof data, "You must pass a type argument to fetchData">>,
+    Expect<Equal<typeof data, "You must pass a type argument to fetchData">>
   ];
 });
