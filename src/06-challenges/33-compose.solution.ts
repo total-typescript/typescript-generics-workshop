d712206b10bdd3b128fbbeaa86b4292a0977b607
Matt Pocock <mattpocockvoice@gmail.com>
2023-02-02 11:11:47 +0000

    Changed 33 solution

--- src/06-challenges/33-compose.solution.ts
+++ src/06-challenges/33-compose.solution.ts
@@ -1,16 +1,22 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-export function compose<T1, T2>(a: (t1: T1) => T2): (t1: T1) => T2;
+export function compose<T1, T2>(func: (t1: T1) => T2): (t1: T1) => T2;
 export function compose<T1, T2, T3>(
-  a: (t1: T1) => T2,
-  b: (t2: T2) => T3,
+  func1: (t1: T1) => T2,
+  func2: (t2: T2) => T3
 ): (t1: T1) => T3;
 export function compose<T1, T2, T3, T4>(
-  a: (t1: T1) => T2,
-  b: (t2: T2) => T3,
-  c: (t2: T3) => T4,
+  func1: (t1: T1) => T2,
+  func2: (t2: T2) => T3,
+  func3: (t3: T3) => T4
 ): (t1: T1) => T4;
+export function compose<T1, T2, T3, T4, T5>(
+  func1: (t1: T1) => T2,
+  func2: (t2: T2) => T3,
+  func3: (t3: T3) => T4,
+  func4: (t4: T4) => T5
+): (t1: T1) => T5;
 export function compose(...funcs: Array<(input: any) => any>) {
   return (input: any) => {
     return funcs.reduce((acc, fn) => fn(acc), input);
@@ -28,7 +34,7 @@ const stringifyThenAddOne = compose(
   String,
   // addOne takes in a number - so it shouldn't be allowed after
   // a function that returns a string!
-  addOne,
+  addOne
 );
 
 it("Should compose multiple functions together", () => {
