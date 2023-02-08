fe3ec16aea21776a38a36c600ba5f9729dc3f257
Matt Pocock <mattpocockvoice@gmail.com>
2023-02-08 11:37:44 +0000

    Added 29.2

---
+++ src/06-challenges/29.2-dynamic-function-arguments.problem.ts
@@ -0,0 +1,46 @@
+import { it } from "vitest";
+
+interface Events {
+  click: {
+    x: number;
+    y: number;
+  };
+  focus: undefined;
+}
+
+export const sendEvent = (event: keyof Events, ...args: any[]) => {
+  // Send the event somewhere!
+};
+
+it("Should force you to pass a second argument when you choose an event with a payload", () => {
+  // @ts-expect-error
+  sendEvent("click");
+
+  sendEvent("click", {
+    // @ts-expect-error
+    x: "oh dear",
+  });
+
+  sendEvent(
+    "click",
+    // @ts-expect-error
+    {
+      y: 1,
+    }
+  );
+
+  sendEvent("click", {
+    x: 1,
+    y: 2,
+  });
+});
+
+it("Should prevent you from passing a second argument when you choose an event without a payload", () => {
+  sendEvent("focus");
+
+  sendEvent(
+    "focus",
+    // @ts-expect-error
+    {}
+  );
+});
---
+++ src/06-challenges/29.2-dynamic-function-arguments.solution.ts
@@ -0,0 +1,51 @@
+import { it } from "vitest";
+
+interface Events {
+  click: {
+    x: number;
+    y: number;
+  };
+  focus: undefined;
+}
+
+export const sendEvent = <TEventKey extends keyof Events>(
+  event: TEventKey,
+  ...args: Events[TEventKey] extends undefined
+    ? []
+    : [payload: Events[TEventKey]]
+) => {
+  // Send the event somewhere!
+};
+
+it("Should force you to pass a second argument when you choose an event with a payload", () => {
+  // @ts-expect-error
+  sendEvent("click");
+
+  sendEvent("click", {
+    // @ts-expect-error
+    x: "oh dear",
+  });
+
+  sendEvent(
+    "click",
+    // @ts-expect-error
+    {
+      y: 1,
+    },
+  );
+
+  sendEvent("click", {
+    x: 1,
+    y: 2,
+  });
+});
+
+it("Should prevent you from passing a second argument when you choose an event without a payload", () => {
+  sendEvent("focus");
+
+  sendEvent(
+    "focus",
+    // @ts-expect-error
+    {},
+  );
+});
